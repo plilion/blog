e19e0d984c292d428ac7feef119ea195f2f90bbf
plilion <plilion@qq.com>
2013-07-16 18:09:48 +0800

    basic page

--- app.js
+++ app.js
@@ -5,7 +5,6 @@
 
 var express = require('express')
   , routes = require('./routes')
-  , user = require('./routes/user')
   , http = require('http')
   , path = require('path');
 
@@ -26,9 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
-
-app.get('/', routes.index);
-app.get('/users', user.list);
+routes(app);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,22 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
-};
+module.exports = function(app){
+    app.get('/',function(req, res){
+        res.render('index', { title: '首页',login:true });
+    });
+    app.get('/login',function(req,res){
+        res.render('login',{title:'登录',login:false});
+    });
+    app.get('/reg',function(req,res){
+        res.render('reg',{title:'注册',login:false});
+    });
+    app.get('/list/:id?',function(req,res){
+        res.render('list',{title:'文章列表',login:false,post:[
+            {id:1,title:'First Post',time:new Date().toString()}
+        ]});
+    });
+    app.get('/post',function(req,res){
+        res.render('post',{title:'发表',login:true});
+    });
+}
